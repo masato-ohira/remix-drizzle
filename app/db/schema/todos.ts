8fcf95102362f052b73bacf9adcfaad7fc36398f
oohira <oohira@ekzm.co.jp>
2024-06-17 14:43:50 +0900

    add:basic-auth

--- app/db/schema/todos.ts
+++ app/db/schema/todos.ts
@@ -9,6 +9,6 @@ import {
 export const todos = sqliteTable('todos', {
   id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
   title: text('title').notNull(),
-  done: integer('done', { mode: 'boolean' }).default(true),
+  done: integer('done', { mode: 'boolean' }).default(false),
   createAt: text('createAt').default(sql`CURRENT_TIMESTAMP`),
 })
